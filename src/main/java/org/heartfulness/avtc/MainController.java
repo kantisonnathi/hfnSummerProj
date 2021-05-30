68262f9e1933bfafb3cdce2bb0fc9197e72a6abe
Kanti Sonnathi <sonnathikanti@gmail.com>
2021-05-30 13:14:38 +0530

    stage 1 done

--- src/main/java/org/heartfulness/avtc/MainController.java
+++ src/main/java/org/heartfulness/avtc/MainController.java
@@ -1,15 +1,53 @@
 package org.heartfulness.avtc;
 
 
+import net.minidev.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @Controller
 public class MainController {
 
     @GetMapping("/main")
-    public String getMain() {
+    public String getMain(ModelMap modelMap) {
         User user = new User();
+        modelMap.put("userEntity", user);
         return "main/phone-num-auth";
     }
+
+    @GetMapping("/test")
+    public String test() {
+        return "main/test";
+    }
+
+    @GetMapping(path="/check/{contactNumber}", produces=MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> sayHello(@PathVariable("contactNumber")String contactNumber) {
+        //Get data from service layer into entityList.
+
+        List<JSONObject> entities = new ArrayList<JSONObject>();
+        JSONObject entity = new JSONObject();
+        if (contactNumber.equals("+917338897712")) {
+            entity.put("phoneNumber", "success");
+        } else {
+            entity.put("phoneNumber", "failure");
+        }
+        entities.add(entity);
+        return new ResponseEntity<Object>(entities, HttpStatus.OK);
+    }
+
+
+
+
 }
--- src/main/java/org/heartfulness/avtc/User.java
+++ src/main/java/org/heartfulness/avtc/User.java
@@ -13,5 +13,83 @@ public class User {
     private Integer receivesCalls;
     private String contactType;
 
+    public String getContactNumber() {
+        return contactNumber;
+    }
 
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public Integer getExtension() {
+        return extension;
+    }
+
+    public void setExtension(Integer extension) {
+        this.extension = extension;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAltContactNumber() {
+        return altContactNumber;
+    }
+
+    public void setAltContactNumber(String altContactNumber) {
+        this.altContactNumber = altContactNumber;
+    }
+
+    public String getAltCountryCode() {
+        return altCountryCode;
+    }
+
+    public void setAltCountryCode(String altCountryCode) {
+        this.altCountryCode = altCountryCode;
+    }
+
+    public Integer getPanel_access() {
+        return panel_access;
+    }
+
+    public void setPanel_access(Integer panel_access) {
+        this.panel_access = panel_access;
+    }
+
+    public Integer getReceivesCalls() {
+        return receivesCalls;
+    }
+
+    public void setReceivesCalls(Integer receivesCalls) {
+        this.receivesCalls = receivesCalls;
+    }
+
+    public String getContactType() {
+        return contactType;
+    }
+
+    public void setContactType(String contactType) {
+        this.contactType = contactType;
+    }
 }

4737f629cb6028b92291bdde2373d11176ab3772
sahithi <sahithi02.ra@gmail.com>
2021-06-13 12:22:06 +0530

    After call

---
+++ src/main/java/org/heartfulness/avtc/model/AfterCallNode.java
@@ -0,0 +1,393 @@
+package org.heartfulness.avtc.model;
+
+import com.google.api.client.util.DateTime;
+
+import javax.validation.constraints.Email;
+import java.net.URL;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.List;
+
+public class AfterCallNode {
+    private Boolean _an;
+    private String _ai;
+    private String _cl;
+    private Integer _cr;
+    private String _cm;
+    private String _cy;
+    private String _ev;
+    private URL _fu;
+    private String _fn;
+    private Timestamp _ts;
+    private Timestamp _ms;
+    private Time _st;
+    private Time _et;
+    private Time _ss;
+    private Integer _ns;
+    private List<String> _se;
+    private Integer _su;
+    private Integer _so;
+    private String _ci;
+    private Time _dr;
+    private Time drm;
+    private String _di;
+    private String _dn;
+    private Integer _ty;
+    private DateTime _Id_rst;
+    private String _id__rr_na;
+    private String  _id_rr_id;//check
+    private String _id_rr_em;
+    private Integer _id_rr_ct;
+    private Integer _id_su;
+    private Time _id_st;
+    private Time _id_et;
+    private Time _id_dr;
+    private String _id_ds;
+    private String _id_did;
+    private String _id_ac;
+    private String _id_tt_na;
+    private String _id_tt_id;
+    private String _id_tt_em;
+
+    public Boolean get_an() {
+        return _an;
+    }
+
+    public void set_an(Boolean _an) {
+        this._an = _an;
+    }
+
+    public String get_ai() {
+        return _ai;
+    }
+
+    public void set_ai(String _ai) {
+        this._ai = _ai;
+    }
+
+    public String get_cl() {
+        return _cl;
+    }
+
+    public void set_cl(String _cl) {
+        this._cl = _cl;
+    }
+
+    public Integer get_cr() {
+        return _cr;
+    }
+
+    public void set_cr(Integer _cr) {
+        this._cr = _cr;
+    }
+
+    public String get_cm() {
+        return _cm;
+    }
+
+    public void set_cm(String _cm) {
+        this._cm = _cm;
+    }
+
+    public String get_cy() {
+        return _cy;
+    }
+
+    public void set_cy(String _cy) {
+        this._cy = _cy;
+    }
+
+    public String get_ev() {
+        return _ev;
+    }
+
+    public void set_ev(String _ev) {
+        this._ev = _ev;
+    }
+
+    public URL get_fu() {
+        return _fu;
+    }
+
+    public void set_fu(URL _fu) {
+        this._fu = _fu;
+    }
+
+    public String get_fn() {
+        return _fn;
+    }
+
+    public void set_fn(String _fn) {
+        this._fn = _fn;
+    }
+
+    public Timestamp get_ts() {
+        return _ts;
+    }
+
+    public void set_ts(Timestamp _ts) {
+        this._ts = _ts;
+    }
+
+    public Timestamp get_ms() {
+        return _ms;
+    }
+
+    public void set_ms(Timestamp _ms) {
+        this._ms = _ms;
+    }
+
+    public Time get_st() {
+        return _st;
+    }
+
+    public void set_st(Time _st) {
+        this._st = _st;
+    }
+
+    public Time get_et() {
+        return _et;
+    }
+
+    public void set_et(Time _et) {
+        this._et = _et;
+    }
+
+    public Time get_ss() {
+        return _ss;
+    }
+
+    public void set_ss(Time _ss) {
+        this._ss = _ss;
+    }
+
+    public Integer get_ns() {
+        return _ns;
+    }
+
+    public void set_ns(Integer _ns) {
+        this._ns = _ns;
+    }
+
+    public List<String> get_se() {
+        return _se;
+    }
+
+    public void set_se(List<String> _se) {
+        this._se = _se;
+    }
+
+    public Integer get_su() {
+        return _su;
+    }
+
+    public void set_su(Integer _su) {
+        this._su = _su;
+    }
+
+    public Integer get_so() {
+        return _so;
+    }
+
+    public void set_so(Integer _so) {
+        this._so = _so;
+    }
+
+    public String get_ci() {
+        return _ci;
+    }
+
+    public void set_ci(String _ci) {
+        this._ci = _ci;
+    }
+
+    public Time get_dr() {
+        return _dr;
+    }
+
+    public void set_dr(Time _dr) {
+        this._dr = _dr;
+    }
+
+    public Time getDrm() {
+        return drm;
+    }
+
+    public void setDrm(Time drm) {
+        this.drm = drm;
+    }
+
+    public String get_di() {
+        return _di;
+    }
+
+    public void set_di(String _di) {
+        this._di = _di;
+    }
+
+    public String get_dn() {
+        return _dn;
+    }
+
+    public void set_dn(String _dn) {
+        this._dn = _dn;
+    }
+
+    public Integer get_ty() {
+        return _ty;
+    }
+
+    public void set_ty(Integer _ty) {
+        this._ty = _ty;
+    }
+
+    public DateTime get_Id_rst() {
+        return _Id_rst;
+    }
+
+    public void set_Id_rst(DateTime _Id_rst) {
+        this._Id_rst = _Id_rst;
+    }
+
+    public String get_id__rr_na() {
+        return _id__rr_na;
+    }
+
+    public void set_id__rr_na(String _id__rr_na) {
+        this._id__rr_na = _id__rr_na;
+    }
+
+    public String get_id_rr_id() {
+        return _id_rr_id;
+    }
+
+    public void set_id_rr_id(String _id_rr_id) {
+        this._id_rr_id = _id_rr_id;
+    }
+
+    public String get_id_rr_em() {
+        return _id_rr_em;
+    }
+
+    public void set_id_rr_em(String _id_rr_em) {
+        this._id_rr_em = _id_rr_em;
+    }
+
+    public Integer get_id_rr_ct() {
+        return _id_rr_ct;
+    }
+
+    public void set_id_rr_ct(Integer _id_rr_ct) {
+        this._id_rr_ct = _id_rr_ct;
+    }
+
+    public Integer get_id_su() {
+        return _id_su;
+    }
+
+    public void set_id_su(Integer _id_su) {
+        this._id_su = _id_su;
+    }
+
+    public Time get_id_st() {
+        return _id_st;
+    }
+
+    public void set_id_st(Time _id_st) {
+        this._id_st = _id_st;
+    }
+
+    public Time get_id_et() {
+        return _id_et;
+    }
+
+    public void set_id_et(Time _id_et) {
+        this._id_et = _id_et;
+    }
+
+    public Time get_id_dr() {
+        return _id_dr;
+    }
+
+    public void set_id_dr(Time _id_dr) {
+        this._id_dr = _id_dr;
+    }
+
+    public String get_id_ds() {
+        return _id_ds;
+    }
+
+    public void set_id_ds(String _id_ds) {
+        this._id_ds = _id_ds;
+    }
+
+    public String get_id_did() {
+        return _id_did;
+    }
+
+    public void set_id_did(String _id_did) {
+        this._id_did = _id_did;
+    }
+
+    public String get_id_ac() {
+        return _id_ac;
+    }
+
+    public void set_id_ac(String _id_ac) {
+        this._id_ac = _id_ac;
+    }
+
+    public String get_id_tt_na() {
+        return _id_tt_na;
+    }
+
+    public void set_id_tt_na(String _id_tt_na) {
+        this._id_tt_na = _id_tt_na;
+    }
+
+    public String get_id_tt_id() {
+        return _id_tt_id;
+    }
+
+    public void set_id_tt_id(String _id_tt_id) {
+        this._id_tt_id = _id_tt_id;
+    }
+
+    public String get_id_tt_em() {
+        return _id_tt_em;
+    }
+
+    public void set_id_tt_em(String _id_tt_em) {
+        this._id_tt_em = _id_tt_em;
+    }
+
+    public Integer get_id_tt_ct() {
+        return _id_tt_ct;
+    }
+
+    public void set_id_tt_ct(Integer _id_tt_ct) {
+        this._id_tt_ct = _id_tt_ct;
+    }
+
+    public String get_id_tt_nr() {
+        return _id_tt_nr;
+    }
+
+    public void set_id_tt_nr(String _id_tt_nr) {
+        this._id_tt_nr = _id_tt_nr;
+    }
+
+    public String get_pm_ky() {
+        return _pm_ky;
+    }
+
+    public void set_pm_ky(String _pm_ky) {
+        this._pm_ky = _pm_ky;
+    }
+
+    private Integer _id_tt_ct;
+    private String _id_tt_nr;
+    private String _pm_ky;//redo
+
+
+}

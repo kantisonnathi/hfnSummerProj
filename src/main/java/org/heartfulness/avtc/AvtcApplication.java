800ef4a15aeb669fa5948dda41256a8e84f6fa40
Kanti Sonnathi <sonnathikanti@gmail.com>
2021-06-16 17:55:51 +0530

    ngrok will finally work on this

--- src/main/java/org/heartfulness/avtc/AvtcApplication.java
+++ src/main/java/org/heartfulness/avtc/AvtcApplication.java
@@ -1,10 +1,24 @@
 package org.heartfulness.avtc;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AvtcApplication {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/private/sessionLogin").allowedOrigins("*");
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AvtcApplication.class, args);

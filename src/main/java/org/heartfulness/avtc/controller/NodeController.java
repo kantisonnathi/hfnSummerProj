d76a4de84a67b84379f9fb09c08b91feed307354
Kanti Sonnathi <sonnathikanti@gmail.com>
2021-06-22 22:46:54 +0530

    in call work

--- src/main/java/org/heartfulness/avtc/controller/NodeController.java
+++ src/main/java/org/heartfulness/avtc/controller/NodeController.java
@@ -82,6 +82,7 @@ public class NodeController {
         if (agents.isEmpty()) {
             JSONObject entity = new JSONObject();
             call.setStatus(CallStatus.DISCONNECTED);
+            this.callRepository.save(call);
             entity.put("action", "tts");
             entity.put("value", "Sorry, there are no community level workers available at this time, please try again " +
                     "later");
@@ -120,6 +121,9 @@ public class NodeController {
     @ResponseBody
     @Produces(MediaType.APPLICATION_JSON)
     public ResponseEntity<?> inCallWebHook(@RequestParam("myoperator") String jsonString) {
+
+        System.out.println("\n\n\n" + jsonString + "\n\n\n");
+
         for (int i = 0; i < jsonString.length(); i++) {
             if (jsonString.charAt(i) == '[' && jsonString.charAt(i-1) == '\"') {
                 jsonString = jsonString.substring(0,i-1) + jsonString.substring(i);
@@ -155,6 +159,10 @@ public class NodeController {
             case 2:
                 //call finished
                 call = this.callRepository.findByCallerAndCallStatus(caller, CallStatus.CONNECTED_TO_AGENT);
+                if (call == null) {
+                    System.out.println("call state 2, call is null\n\n\n");
+                    break;
+                }
                 call.setStatus(CallStatus.DISCONNECTED);
                 agent = call.getAgent();
                 agent.setStatus(AgentStatus.ONLINE); //the logger should not be updated here
@@ -169,9 +177,17 @@ public class NodeController {
             case 6:
                 // agent picked up
                 call = this.callRepository.findByCallerAndCallStatus(caller, CallStatus.AWAITING_CONNECTION_TO_AGENT);
+                if (call == null) {
+                        System.out.println("call state 6, call is null\n\n\n");
+                        break;
+                }
                 call.setStatus(CallStatus.CONNECTED_TO_AGENT);
                 String phoneNumber = inCallNode.getUsers().get(0);
                 agent = this.agentRepository.findByContactNumber(phoneNumber);
+                if (agent == null) {
+                    System.out.println("Agent is null, state 6\n\n\n\n");
+                    break;
+                }
                 call.setAgent(agent);
                 agent.setStatus(AgentStatus.IN_CALL);
                 Set<Agent> leasedAgents = call.getLeasing();
@@ -205,7 +221,7 @@ public class NodeController {
         List<JSONObject> entities = new ArrayList<>();
         Gson gson=new Gson();
         AfterCallNode afterCallNode=gson.fromJson(jsonString,AfterCallNode.class);
-       Call call=new Call();
+        Call call=new Call();
         Caller caller=callerRepository.findByContactNumber(afterCallNode.get_cr());
         call.setCaller(caller);
         call.setLocation(afterCallNode.get_se());
